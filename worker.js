338558227b605928ce9e034525e2075a76229fff
Phat Wangrungarun <phatograph@gmail.com>
2012-09-18 13:38:16 +0700

    Change to 5 mins

--- worker.js
+++ worker.js
@@ -1,7 +1,7 @@
 var cronJob = require('cron').CronJob;
 var i = 0;
 var job = new cronJob({
-  cronTime: '1 * * * * *',
+  cronTime: '5 * * * * *',
   onTick: function() {
     console.log(new Date());
   },
