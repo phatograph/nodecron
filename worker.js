0f0466c074ac6ba358dc650d5f0ace51bda8fc85
Phat Wangrungarun <phatograph@gmail.com>
2012-09-18 13:55:47 +0700

    Change to worker node

--- worker.js
+++ worker.js
@@ -1,7 +1,7 @@
 var cronJob = require('cron').CronJob;
 var i = 0;
 var job = new cronJob({
-  cronTime: '5 * * * * *',
+  cronTime: '1 * * * * *',
   onTick: function() {
     console.log(new Date());
   },
